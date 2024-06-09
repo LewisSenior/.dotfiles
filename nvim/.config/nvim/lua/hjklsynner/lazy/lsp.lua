999145ddf97830b35efedff270d5c8290d6bbc52
Lewis Senior <lewis@se.nior.uk>
2024-06-09 20:36:48 +0100

    restructure for easier plugin config management

---
+++ nvim/.config/nvim/lua/hjklsynner/lazy/lsp.lua
@@ -0,0 +1,9 @@
+return {
+	{'neovim/nvim-lspconfig',
+		dependencies = {
+			'williamboman/mason.nvim' }
+	},
+	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-nvim-lsp',
+	'williamboman/mason-lspconfig.nvim',
+}
---
+++ nvim/.config/nvim/lua/hjklsynner/lazy/telescope.lua
@@ -0,0 +1,4 @@
+return
+	{'nvim-telescope/telescope.nvim', version = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
